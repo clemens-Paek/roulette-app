0d21f5cb8be0f5ad139ba34367b63a5482bb7101
pshyun <pshyun0220@gmail.com>
2016-08-07 22:10:21 +0900

    mobile, tablet 식별방법 java config로 변경

---
+++ src/main/java/com/pshyun/reoulette/config/DeviceConfiguration.java
@@ -0,0 +1,46 @@
+package com.pshyun.reoulette.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mobile.device.DeviceHandlerMethodArgumentResolver;
+import org.springframework.mobile.device.DeviceResolverHandlerInterceptor;
+import org.springframework.mobile.device.view.LiteDeviceDelegatingViewResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class DeviceConfiguration {
+	
+	@Bean
+	public DeviceResolverHandlerInterceptor deviceResolverHandlerInterceptor() {
+		return new DeviceResolverHandlerInterceptor();
+	}
+	
+	@Bean
+	public DeviceHandlerMethodArgumentResolver deviceHandlerMethodArgumentResolver() {
+		return new DeviceHandlerMethodArgumentResolver();
+	}
+	
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(deviceResolverHandlerInterceptor());
+	}
+	
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		argumentResolvers.add(deviceHandlerMethodArgumentResolver());
+	}
+	
+	@Bean
+	public LiteDeviceDelegatingViewResolver liteDeviceAwareViewResolver() {
+		InternalResourceViewResolver delegate = new InternalResourceViewResolver();
+		delegate.setPrefix("/WEB-INF/views/");
+		delegate.setSuffix(".jsp");
+		LiteDeviceDelegatingViewResolver resolver = new LiteDeviceDelegatingViewResolver(delegate);
+		resolver.setMobilePrefix("mobile/");
+		resolver.setTabletPrefix("tablet/");
+		return resolver;
+	}
+	
+}
--- src/main/java/com/pshyun/reoulette/home/control/HomeController.java
+++ src/main/java/com/pshyun/reoulette/home/control/HomeController.java
@@ -1,10 +1,7 @@
 package com.pshyun.reoulette.home.control;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.mobile.device.Device;
-import org.springframework.mobile.device.DeviceUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,13 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 	
 	@RequestMapping("/")
-	public String index(HttpServletRequest request) {
-		//사용자 접근 디바이스타입에 따른 분기처리 
-		Device device = DeviceUtils.getCurrentDevice(request);
-		System.out.println("@@ device is mobile? : " + device.isMobile());
-
+	public String home(Device device) {
+		System.out.println("@@ device is mobile / tablet? : " + device.isMobile() + " / " + device.isTablet());
 		//모바일 디바이스로 접속했다면 
 		if (device.isMobile()) {
+			return "mindex";
+		} else if (device.isTablet()) {
 			return "mindex";
 		} else {
 			return "mindex";
